c1e9db8cedda632fc8fc8553659bc30812e899ad
Claudia Rey Izquierdo <97339933+Clauane@users.noreply.github.com>
2025-01-05 19:24:39 +0100

    Add files via upload

---
+++ src/main/java/es/upsa/bbdd2/database/application/Impl/DaoImpl.java
@@ -0,0 +1,4 @@
+package es.upsa.bbdd2.database.application.Impl;
+
+public class DaoImpl {
+}
---
+++ src/main/java/es/upsa/bbdd2/database/domain/entities/UnidadMedida.java
@@ -0,0 +1,6 @@
+package es.upsa.bbdd2.database.domain.entities;
+
+public enum UnidadMedida {
+
+    GRAMOS, UNIDADES, CENTILITROS
+}
