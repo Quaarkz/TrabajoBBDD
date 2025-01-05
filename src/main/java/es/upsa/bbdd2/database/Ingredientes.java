1f53693ef1029219266137954a4d74f0a9745456
Quaarkz <Quaarkz@gmail.com>
2025-01-05 18:42:24 +0100

    Commit inicial

---
+++ src/main/java/es/upsa/bbdd2/database/Ingredientes.java
@@ -0,0 +1,20 @@
+package es.upsa.bbdd2.database;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(setterPrefix = "with")
+
+
+public class Ingredientes {
+
+    private int id;
+    private String ingrediente;
+
+}
---
+++ src/main/java/es/upsa/bbdd2/database/Platos.java
@@ -0,0 +1,21 @@
+package es.upsa.bbdd2.database;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(setterPrefix = "with")
+
+
+public class Platos {
+    private int id;
+    private String nombre;
+    private String descripcion;
+    private float precio;
+    private EnumeracionTipo tipo; //hacer enum
+}
