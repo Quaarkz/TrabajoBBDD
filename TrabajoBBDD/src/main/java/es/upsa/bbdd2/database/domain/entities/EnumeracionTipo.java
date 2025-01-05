58b48ea65b893f57791ae7090ede7679c1fed77b
Claudia Rey Izquierdo <97339933+Clauane@users.noreply.github.com>
2025-01-05 19:27:07 +0100

    Add files via upload

---
+++ TrabajoBBDD/src/main/java/es/upsa/bbdd2/database/domain/entities/EnumeracionTipo.java
@@ -0,0 +1,6 @@
+package es.upsa.bbdd2.database.domain.entities;
+
+public enum EnumeracionTipo {
+
+    ENTRANTE, PRINCIPAL, POSTRE, INFANTIL
+}
---
+++ TrabajoBBDD/src/main/java/es/upsa/bbdd2/database/domain/entities/Ingrediente.java
@@ -0,0 +1,23 @@
+package es.upsa.bbdd2.database.domain.entities;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(setterPrefix = "with")
+
+
+public class Ingrediente {
+
+    private int id;
+    private String nombre;
+    private List<CantidadIngrediente> cantidades;
+
+}
