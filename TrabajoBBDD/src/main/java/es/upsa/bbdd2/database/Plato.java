d39eefae09add2a2656453befd3c7a1b3aa5f067
Claudia Rey Izquierdo <97339933+Clauane@users.noreply.github.com>
2025-01-05 18:59:40 +0100

    Add files via upload

---
+++ TrabajoBBDD/src/main/java/es/upsa/bbdd2/database/Plato.java
@@ -0,0 +1,26 @@
+package es.upsa.bbdd2.database;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(setterPrefix = "with")
+
+
+public class Plato {
+
+    private int id;
+    private String nombre;
+    private String descripcion;
+    private float precio;
+    private EnumeracionTipo tipo;
+    private List<Ingrediente> ingredientes;//hacer enum
+
+}
---
+++ TrabajoBBDD/src/main/java/es/upsa/bbdd2/database/UnidadMedida.java
@@ -0,0 +1,6 @@
+package es.upsa.bbdd2.database;
+
+public enum UnidadMedida {
+
+    GRAMOS, UNIDADES, CENTILITROS
+}
